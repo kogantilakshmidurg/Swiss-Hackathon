ab42ea21ff3853a8d288f2ab201ac7d0c2e10efb
kogantilakshmidurg <48591344+kogantilakshmidurg@users.noreply.github.com>
2019-08-11 14:47:12 +0530

    Create Coffee_Shop.cpp

---
+++ Coffee_Shop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int x,p,s=0;
+    cin>>x>>p; //x->price of coffee; p->discount rate
+    p=100-p;
+    while(x>0)
+    {
+        s=s+x; //s->calculating amount required according to the price and dicsount
+        x=(p*x)/100; //x->price after applying discount
+    }
+    cout<<s; //s->amount necessary to order for free
+	return 0;
+}
